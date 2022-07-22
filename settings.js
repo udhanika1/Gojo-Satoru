98f8c8041b32f9b985ecd3ef7c46e60f06890d3e
▁ ▂ ▄ ▅ ▆ ▇ ⓤⓓⓗⓐⓝⓘⓚⓐ Ⓓⓘⓢⓐⓝⓐⓨⓐⓚⓐ ▇ ▆ ▅ ▄ ▂ ▁ 1 <107736673+udhanika1@users.noreply.github.com>
2022-07-22 17:37:15 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,18 +19,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
-global.botname = 'Gojo-Satoru'
-global.footer = '©NexusNwInc.'
-global.ig = 'https://github.com/nexusNw'
+global.owner = ['https://wa.me/94704638406']
+global.premium = ['https://wa.me/94704638406']
+global.ownernomer = 'https://wa.me/94704638406'
+global.ownername = 'SL UD'
+global.botname = 'SL UD Bot'
+global.footer = '©udmods.'
+global.ig = 'https://sl-ud-mods.yolasite.com/'
 global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
-global.packname = 'Gojo-Satoru'
-global.author = '🐦Star Boy Nexus'
+global.sc = 'https://sl-ud-mods.yolasite.com/'
+global.myweb = 'https://youtube.com/channel/UC4eIQOT9JdznytsprIUhE-w'
+global.packname = 'SL UD Bot'
+global.author = 'SL UD'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
